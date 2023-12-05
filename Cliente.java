79ccb76fec5d48ee02250c94099bc3c0d863b26c
Douglas <douglas403567@hotmail.com>
2023-12-05 16:40:47 -0300

    Initial commit git config -- global user.Email "douglas403567@hotmail.com" git config -- global user.nome "Douglas josé marques"

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    private String name;
+    private int age;
+
+    public Cliente(String name, int idade) {
+        this.name = name;
+        this.age = idade;
+    }
+
+    public void valorFinalPassagem()
+}
